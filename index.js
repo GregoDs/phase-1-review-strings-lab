b1a5df4af3524953ae231962b99f3483b1380a56
Gregory Kago <115575284+GregoDs@users.noreply.github.com>
2024-01-24 14:43:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 // Write your code in this file!
-const currentUser = "John Doe"; // You can replace this with the actual name
 
-// Function to generate a welcome message
-function welcomeMessage() {
-  return "Welcome to Flatbook, " + currentUser + "!";
-}
+const currentUser = 'Grace Hopper';
 
-// Function to generate an excited welcome message
-function excitedWelcomeMessage() {
-  return "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
-}
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-// Function to generate a short greeting
-function shortGreeting() {
-  return "Welcome, " + currentUser.charAt(0) + "!";
-}
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
+const shortGreeting = `Welcome, ${currentUser}`;
